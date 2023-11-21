8eb5cfbf94c607d8c5ce35f4e8e7a5fac1add842
Joe-W <80954812+J-Wll@users.noreply.github.com>
2023-11-21 13:10:12 +0000

    Comments for planning and Map.h added

--- AdventureGame/Location.h
+++ AdventureGame/Location.h
@@ -1,9 +1,10 @@
 #pragma once
 
+// list of monsters at that location, like 3 per?, dynamic array with monsters being removed/added?
+// also a list of items/equipment (objects) 
+// monsters and or equip could be randomly generated/selected from a bigger list
+
 class Location {
 	string name;
 	string type;
-	// list of monsters at that location, like 3 per?
-	// also a list of items/equipment (objects) 
-	// monsters and or equip could be randomly generated from a bigger list
 };
---
+++ AdventureGame/Map.h
@@ -0,0 +1,6 @@
+#pragma once
+// class holding a list of locations, after a location is completed it is removed from the array, shows memory management, length of locations could be varied based on something like selected difficulty
+// could also store the next/previous locations in a doubly linked list
+class Map {
+
+};
