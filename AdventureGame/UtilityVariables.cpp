f8b1e6c4a1026f564a828f19884275810cc27991
Joe-W <80954812+J-Wll@users.noreply.github.com>
2024-01-01 14:27:55 +0000

    Switched to using non testing intervals of Sleep

--- AdventureGame/UtilityVariables.cpp
+++ AdventureGame/UtilityVariables.cpp
@@ -7,11 +7,11 @@ std::string greenColour = "\033[1;92m";
 std::string defaultColour = "\033[1;97m";
 
 // Ms, used with the Sleep function to make output less overwhelming
-/*int STDSLEEPTIME = 1000;
+int STDSLEEPTIME = 1000;
 int SMLSLEEPTIME = 300;
-int LRGSLEEPTIME = 2000;*/
+int LRGSLEEPTIME = 2000;
 
 // Testing values (shorter time so script can play through quickly)
-int STDSLEEPTIME = 1;
+/*int STDSLEEPTIME = 1;
 int SMLSLEEPTIME = 1;
-int LRGSLEEPTIME = 1;
+int LRGSLEEPTIME = 1;*/
