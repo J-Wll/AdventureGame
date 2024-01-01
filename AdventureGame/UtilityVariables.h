e41e1a58bb02a4711e72eca6873ee4f6a686185e
Joe-W <80954812+J-Wll@users.noreply.github.com>
2024-01-01 14:30:39 +0000

    Minor comment change

--- AdventureGame/UtilityVariables.h
+++ AdventureGame/UtilityVariables.h
@@ -2,11 +2,12 @@
 #include <string>
 #include "UtilityVariables.h"
 
-// https://learn.microsoft.com/en-us/windows/console/console-virtual-terminal-sequences
+// colour codes for terminal output https://learn.microsoft.com/en-us/windows/console/console-virtual-terminal-sequences
 extern std::string redColour;
 extern std::string greenColour;
 extern std::string defaultColour;
-// Ms, used to make output less overwhelming
+
+// Ms, used with the Sleep function to make output less overwhelming
 extern int STDSLEEPTIME;
 extern int SMLSLEEPTIME;
 extern int LRGSLEEPTIME;
